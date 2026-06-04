b41627b256a35d81a7a5137fea0e80aabf811c54
Henrique Barros <henriquebarrosx@gmail.com>
2026-06-04 14:48:34 -0300

    chore: adiciona repositories de account e transaction

---
+++ src/main/java/com/cajuice/app/repository/TransactionRepository.java
@@ -0,0 +1,11 @@
+package com.cajuice.app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cajuice.app.domain.entity.Transaction;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+}
